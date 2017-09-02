5986dea1ca8be6bc2d3e132d6a261e14454b8929
laurentperez <l.laurent.p@gmail.com>
2017-09-02 13:18:49 +0200

    bug

--- src/test/java/com/test/MyTest.java
+++ src/test/java/com/test/MyTest.java
@@ -2,11 +2,15 @@ package com.test;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class MyTest {
 
     @Test
     public void meh() {
         MyObject o = new MyObject();
+        o.setId("meh");
+        assertEquals(o.getId(), "");
         System.out.println(o);
         YourClass y = new YourClass();
         y.yourMethodAround();
