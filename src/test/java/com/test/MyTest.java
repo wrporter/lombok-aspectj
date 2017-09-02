5f2c7273060dc7408b42f5a574d0291e3f773a27
laurentperez <l.laurent.p@gmail.com>
2017-09-02 13:23:06 +0200

    works

--- src/test/java/com/test/MyTest.java
+++ src/test/java/com/test/MyTest.java
@@ -4,13 +4,16 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ * You need to set Intellij compiler to Javac instead of Ajc in Preferences/Build/Compiler
+ */
 public class MyTest {
 
-    @Test
+    @Test(expected = RuntimeException.class)
     public void meh() {
         MyObject o = new MyObject();
         o.setId("meh");
-        assertEquals(o.getId(), "");
+        assertEquals(o.getId(), "meh");
         System.out.println(o);
         YourClass y = new YourClass();
         y.yourMethodAround();
